6b9830fdaab8d843ff884f64e733d67b6b69a633
yuhuashang <yuhuashang@aliyun.cfom>
2018-05-25 10:39:29 +0800

    python全站

---
+++ venv/Include/com/yuhuashang/demo/ceshi.py
@@ -0,0 +1,8 @@
+name = 'yang'
+name1 = 'scanf'
+print(name)
+print(name.__len__())
+print(name.find('an'))
+a = 'scan' in name1
+print(a)
+
---
+++ venv/Include/com/yuhuashang/demo/ceshi2.py
@@ -0,0 +1,15 @@
+test = 'alex'
+v = test.capitalize()
+print(v)
+
+v1 = test.casefold()
+print(v1)
+v2 = test.lower()
+print(v2)
+
+v3 = test.center(20, '中')
+print(v3)
+
+demo = 'ALexajhuhwgcw'
+r1 = demo.count('ex')
+print(r1)
