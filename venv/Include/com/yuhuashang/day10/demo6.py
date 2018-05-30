5f259e0bbcf5e8f34715a4a87f85afb194a7bd98
yuhuashang <yuhuashang@aliyun.cfom>
2018-05-30 18:43:10 +0800

    python全站

---
+++ venv/Include/com/yuhuashang/day10/demo6.py
@@ -0,0 +1,44 @@
+# test = "alex"
+# tes1 = "uile"
+# # 最长公共子序列
+# r = test.strip(tes1)
+# print(r)
+
+######6个基本魔法#######
+# join
+# split
+# find
+# strip
+# upper
+# lower
+
+####灰魔法####
+#
+# # 索引，
+# test = "alex"
+# r = test[2]
+# print(r)
+
+# 切片
+# test= "alex"
+# r = test[0:2]
+# r1 = test[0:-1]
+# print(r)
+# print(r1)
+#
+# # len获取当前字符串中由几个字符组成
+# r2 = len(test)
+# print(r2)
+# # 对其他数据类型也可用
+#
+# test= "丁建文妹子有种冲我来"
+# index = 0
+# while index < len(test):
+#     v = test[index]
+#     print(v)
+#
+#     index += 1
+# print("=======")
+#
+# for z in test:
+#     print(z)
