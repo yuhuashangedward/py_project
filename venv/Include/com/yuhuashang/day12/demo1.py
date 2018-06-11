dd74d892961d2d57005efff5962ce4eb242447fa
yuhuashang <yuhuashang@aliyun.cfom>
2018-06-11 14:31:48 +0800

    python全站

---
+++ venv/Include/com/yuhuashang/day12/demo1.py
@@ -0,0 +1,6 @@
+test = 123
+print(id(test))
+print(isinstance(test, int))
+print(issubclass(str, object))
+print(issubclass(str, int))
+print()
---
+++ venv/Include/com/yuhuashang/day12/demo2.py
@@ -0,0 +1,6 @@
+import sys
+print('default encoding:', sys.getdefaultencoding())
+
+print('file system encoding:', sys.getfilesystemencoding())
+
+print('stdout encoding:', sys.stdout.encoding)
