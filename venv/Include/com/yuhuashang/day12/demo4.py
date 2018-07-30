183f0d8ded580cc7fb664311468d13bfb78ddc26
yuhuashang <yuhuashang@aliyun.com>
2018-07-30 15:31:38 +0800

    python全站

--- venv/Include/com/yuhuashang/day12/demo4.py
+++ venv/Include/com/yuhuashang/day12/demo4.py
@@ -1,10 +1,17 @@
 list  # 类
 
-li = [1, 12, 4, "alex", "age"]
-print(li)
-print(li[3])
-print(li[2:4])
-print(li[2:-1])
-for item in li:
-    print(item)
-list
+# li = [1, 12, 4, "alex", "age"]
+# print(li)
+# print(li[3])
+# print(li[2:4])
+# print(li[2:-1])
+# for item in li:
+#     print(item)
+# list
+
+tu = (11, 22, 33, 44,)
+
+tu[1] = 44
+print(tu)
+
+tuple
