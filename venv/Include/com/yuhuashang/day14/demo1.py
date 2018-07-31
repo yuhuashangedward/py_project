6da4d528f2796fcb9238b8143b2848eb5a288336
yuhuashang <yuhuashang@aliyun.com>
2018-07-31 15:13:33 +0800

    python全站

---
+++ venv/Include/com/yuhuashang/day14/demo1.py
@@ -0,0 +1,13 @@
+
+
+s = {1, 2, 3, '1'}
+s.add('3')
+print(s)
+
+s.pop()
+print(s)
+
+s.discard(24)
+print(s)
+s.remove(23)
+print(s)
---
+++ venv/Include/com/yuhuashang/day14/demo2.py
@@ -0,0 +1,11 @@
+# p_s = {'szw', 'lcg', 'lnh'}
+# l_s = {'szw', 'zws'}
+# print(p_s.intersection(l_s))
+
+# p_s = {'szw', 'lcg', 'lnh'}
+# l_s = {'szw', 'zws', 'sb'}
+# print(p_s.union(l_s))
+
+p_s = {'szw', 'lcg', 'lnh'}
+l_s = {'szw', 'zws', 'sb'}
+print(p_s-l_s)
