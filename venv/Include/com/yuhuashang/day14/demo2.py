2f667933625c319b6918fdcd62688f39d268bc4f
yuhuashang <yuhuashang@aliyun.com>
2018-08-01 15:06:22 +0800

    python全站

--- venv/Include/com/yuhuashang/day14/demo2.py
+++ venv/Include/com/yuhuashang/day14/demo2.py
@@ -1,11 +1,61 @@
-# p_s = {'szw', 'lcg', 'lnh'}
-# l_s = {'szw', 'zws'}
-# print(p_s.intersection(l_s))
+# # p_s = {'szw', 'lcg', 'lnh'}
+# # l_s = {'szw', 'zws'}
+# # print(p_s.intersection(l_s))
+#
+# # p_s = {'szw', 'lcg', 'lnh'}
+# # l_s = {'szw', 'zws', 'sb'}
+# # print(p_s.union(l_s))
+#
+# # p_s = {'szw', 'lcg', 'lnh'}
+# # l_s = {'szw', 'zws', 'sb'}
+# #
+# # print(p_s.difference(l_s))
+# # print(p_s-l_s)
+#
+#
+#
+#
+# # p_s = {'szw', 'lcg', 'lnh'}
+# # l_s = {'szw', 'zws', 'sb'}
+# # ### p_s = p_s.difference(l_s)
+# # print(p_s.difference_update(l_s))
+# # print(p_s)
+# #
+# #
+# # p_s = {'szw', 'lcg', 'lnh'}
+# # l_s = {'szw', 'zws', 'sb'}
+# # print(p_s.isdisjoint(l_s))
+#
+#
+# # p_s = {'szw', 'lcg', 'lnh', 'zws', 'sb'}
+# # l_s = {'szw', 'zws', 'sb'}
+# # print(p_s.issubset(l_s))
+# # print(l_s.issubset(p_s))
+#
+# #
+# # p_s = {'szw', 'lcg', 'lnh', 'zws', 'sb'}
+# # l_s = {'szw', 'zws', 'sb'}
+# # print(p_s.issuperset(l_s))
+# # # print(l_s.issuperset(p_s))
+#
+# s1 = {1, 2, 4}
+#
+# s2 = {1, 2, 3}
+#
+# s1.update(s2)
+#
+# print(s1)
+#
+# s = forzenset('hello')
+#
+#
+# names = ['alex', 'alex', 'wupeiqi']
+# names = list(set(names))
+# print(names)
 
-# p_s = {'szw', 'lcg', 'lnh'}
-# l_s = {'szw', 'zws', 'sb'}
-# print(p_s.union(l_s))
+msg = "i am \033[42;1m%(name)+60s\033[0m my hobby is alex" %{"name":"lhf"}
+print(msg)
 
-p_s = {'szw', 'lcg', 'lnh'}
-l_s = {'szw', 'zws', 'sb'}
-print(p_s-l_s)
+print('root','x','0','0',sep=':')
+
+
