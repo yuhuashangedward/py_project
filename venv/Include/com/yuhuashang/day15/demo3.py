7625036dac27420149fb69271a0f1ebb60d7fed4
Edward Wang <yuhuashang@aliyun.com>
2018-08-06 14:16:52 +0800

    python全栈

---
+++ venv/Include/com/yuhuashang/day15/demo3.py
@@ -0,0 +1,17 @@
+NAME = '海风'
+
+def huangwei():
+    name = "黄伟"
+    print(name)
+    def liuyang():
+        name = "刘洋"
+        print(name)
+        def nulige():
+            name = "胡志华"
+            print(name)
+        print(name)
+        nulige()
+    liuyang()
+    print(name)
+
+huangwei()
